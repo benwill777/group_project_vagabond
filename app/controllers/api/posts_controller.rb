9c98ef45fa4e0f161f53536ce6c36ecb5aaf32ef
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-05-21 11:22:13 -0400

    configured posts index route

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,16 +1,21 @@
 class Api::PostsController < ApplicationController
   def index
+    @posts = City.find(params[:city_id]).posts
+        render json: @posts
   end
 
   def show
+   
   end
 
   def delete
+
   end
 
   def update
   end
 
   def create
+
   end
 end
