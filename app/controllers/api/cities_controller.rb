ae3fe4d5f17bba2fb74d4725ea47f3b5b199aa70
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-05-21 10:56:28 -0400

    created cities controller

---
+++ app/controllers/api/cities_controller.rb
@@ -0,0 +1,7 @@
+class Api::CitiesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,34 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'cities/index'
+    get 'cities/show'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+  namespace :api do
+    get 'cities/index'
+    get 'cities/show'
+  end
+    namespace :api do
+      get 'posts/index'
+      get 'posts/show'
+      get 'posts/delete'
+      get 'posts/update'
+      get 'posts/create'
+    end
+    namespace :api do
+      get 'cities/index'
+      get 'cities/show'
+      get 'cities/delete'
+      get 'cities/update'
+      get 'cities/create'
+    end
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    namespace :api do
+      resources :cities do
+        resources :posts
+      end
+    end
+  end
+  
 end
