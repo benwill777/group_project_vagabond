6b4249c90c6d5c71a476258276466d6774fb4af0
benwill777 <t.bennettwilliamsjr@gmail.com>
2018-05-21 11:02:50 -0400

    configured city controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
   namespace :api do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/delete'
+    get 'posts/update'
+    get 'posts/create'
+  end
+  namespace :api do
     get 'cities/index'
     get 'cities/show'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
+  namespace :api do
+    get 'posts/index'
+    get 'posts/show'
+    get 'posts/delete'
+    get 'posts/update'
+    get 'posts/create'
+  end
   namespace :api do
     get 'cities/index'
     get 'cities/show'
